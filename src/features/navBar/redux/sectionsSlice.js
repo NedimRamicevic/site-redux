15e45ef103b6fb2594740ea5f6388a9f77d925cb
Nedim <mr.ramicevic@gmail.com>
2022-06-17 12:35:51 +0300

    counter,section Slice added

---
+++ src/features/navBar/redux/sectionsSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const sectionsSlice = createSlice({
+  name: "sections",
+  initialState: [],
+  reducers: {
+    addSection: (state, action) => {
+      return [...state, action.payload];
+    },
+    removeSection: (state, action) => {
+      return state.filter((section) => section.id !== action.payload);
+    },
+  },
+});
+
+export const { addSection, removeSection } = sectionsSlice.actions;
+export default sectionsSlice.reducer;
