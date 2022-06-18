6e44b4da31ff1dd4c24ab08bd7e557f2f921544a
Nedim <mr.ramicevic@gmail.com>
2022-06-18 16:09:01 +0300

    links

--- src/features/navBar/components/navBar.js
+++ src/features/navBar/components/navBar.js
@@ -67,7 +67,9 @@ function NAvbar() {
             <li>
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => dispatch(addSection("naber"))}
+                onClick={() =>
+                  dispatch(addSection({ name: "naber", link: "naber" }))
+                }
               >
                 Ekle
               </button>
