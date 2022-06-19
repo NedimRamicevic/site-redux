60ecf73878c51079bc1803ba07b0bf5c8b927777
Nedim <mr.ramicevic@gmail.com>
2022-06-19 07:04:02 +0300

    done

--- src/features/body/components/content.js
+++ src/features/body/components/content.js
@@ -2,9 +2,13 @@ import React from "react";
 
 function Content({ page }) {
   return (
-    <div className="Content">
+    <div className="items-center">
       <h1>{page.name}</h1>
-      <h3>{page.content}</h3>
+      <div class="pt-6 md:p-8 text-center md:text-center space-y-4">
+        <blockquote>
+          <p class="text-lg font-medium">{page.content}</p>
+        </blockquote>
+      </div>
     </div>
   );
 }
