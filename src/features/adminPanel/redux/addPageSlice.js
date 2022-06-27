2e9043f37786d5a31c70bb7f5a3294b151563200
Nedim <mr.ramicevic@gmail.com>
2022-06-27 12:23:49 +0300

    x

--- src/features/adminPanel/redux/addPageSlice.js
+++ src/features/adminPanel/redux/addPageSlice.js
@@ -3,9 +3,44 @@ import { createSlice } from "@reduxjs/toolkit";
 const addPageSlice = createSlice({
   name: "addPage",
   initialState: [
-    { name: "Home", title: "title", content: "content", link: "home" },
-    { name: "About", title: "title", content: "content", link: "about" },
-    { name: "Contact", title: "title", content: "content", link: "contact" },
+    {
+      name: "Home",
+      content: [
+        {
+          title: "title",
+          img: "https://hoteliilidza.ba/wp-content/uploads/2017/02/mmm.jpg",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tincidunt ex. Aenean feugiat justo ut mi posuere varius. Maecenas nunc est, fringilla id nisl ut, mollis fermentum lacus. Nunc posuere, diam mattis maximus pharetra, ligula elit pulvinar augue, in feugiat sem massa quis dolor. Nam quis diam dui. Integer malesuada condimentum dolor, nec feugiat justo vestibulum at. Nunc sodales nisl eget lacus facilisis suscipit. Fusce quis placerat felis. Cras faucibus accumsan ante ac interdum. In hac habitasse platea dictumst. Nunc et ipsum ut justo rutrum pulvinar. Vivamus eget hendrerit magna. Nunc ut ullamcorper erat.",
+        },
+      ],
+      link: "home",
+    },
+    {
+      name: "About",
+      title: "title",
+      content: [
+        {
+          title: "title",
+          img: "https://via.placeholder.com/150",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tincidunt ex. Aenean feugiat justo ut mi posuere varius. Maecenas nunc est, fringilla id nisl ut, mollis fermentum lacus. Nunc posuere, diam mattis maximus pharetra, ligula elit pulvinar augue, in feugiat sem massa quis dolor. Nam quis diam dui. Integer malesuada condimentum dolor, nec feugiat justo vestibulum at. Nunc sodales nisl eget lacus facilisis suscipit. Fusce quis placerat felis. Cras faucibus accumsan ante ac interdum. In hac habitasse platea dictumst. Nunc et ipsum ut justo rutrum pulvinar. Vivamus eget hendrerit magna. Nunc ut ullamcorper erat.",
+        },
+      ],
+      link: "about",
+    },
+    {
+      name: "Contact",
+      title: "title",
+      content: [
+        {
+          title: "title",
+          img: "https://via.placeholder.com/150",
+          content:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tincidunt ex. Aenean feugiat justo ut mi posuere varius. Maecenas nunc est, fringilla id nisl ut, mollis fermentum lacus. Nunc posuere, diam mattis maximus pharetra, ligula elit pulvinar augue, in feugiat sem massa quis dolor. Nam quis diam dui. Integer malesuada condimentum dolor, nec feugiat justo vestibulum at. Nunc sodales nisl eget lacus facilisis suscipit. Fusce quis placerat felis. Cras faucibus accumsan ante ac interdum. In hac habitasse platea dictumst. Nunc et ipsum ut justo rutrum pulvinar. Vivamus eget hendrerit magna. Nunc ut ullamcorper erat.",
+        },
+      ],
+      link: "contact",
+    },
   ],
   reducers: {
     addPage: (state, action) => {
--- src/features/body/components/content.js
+++ src/features/body/components/content.js
@@ -2,15 +2,27 @@ import React from "react";
 
 function Content({ page }) {
   return (
-    <div className="p-12 items-center">
-      <div className=" pt-6 bg-cyan-500 rounded-3xl">
-        <h1>{page.name}</h1>
-        <div class="pt-6 md:p-8 text-center md:text-center space-y-4 ">
-          <blockquote>
-            <p class="text-lg font-medium">{page.content}</p>
-          </blockquote>
+    <div>
+      {page.content.map((content) => (
+        <div className="p-12 items-center">
+          <div className=" p-6 bg-cyan-500 rounded-3xl grid grid-cols-3">
+            <div
+              className="bg-cover rounded-xl col-span-1"
+              style={{ backgroundImage: `url(${content.img})` }}
+            ></div>
+            <div className=" col-span-2">
+              <h1 className=" text-center">{page.name}</h1>
+              <div class="pt-6 md:p-8 text-center md:text-center space-y-4 ">
+                <blockquote>
+                  <p class="text-lg font-medium text-justify">
+                    {content.content}
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
