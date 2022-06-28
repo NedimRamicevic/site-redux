56b4062a19805adff41c98c1a0778f4fae5980ea
Nedim <mr.ramicevic@gmail.com>
2022-06-28 10:01:49 +0300

    combineStores

---
+++ src/features/adminPanel/models/componentModel.js
@@ -0,0 +1,11 @@
+class ComponentModel {
+  constructor(name, image, title, content, page) {
+    this.name = name;
+    this.image = image;
+    this.title = title;
+    this.content = content;
+    this.page = page;
+  }
+}
+
+export default ComponentModel;
--- src/features/adminPanel/redux/addComponentSlice.js
+++ src/features/adminPanel/redux/addComponentSlice.js
@@ -1,16 +1,25 @@
 // create ComponentSlice
 import { createSlice } from "@reduxjs/toolkit";
+import { ComponentModel } from "../../models/componentModel";
 
 export const addComponentSlice = createSlice({
   name: "addComponent",
   initialState: {
     components: [
-      {
-        name: "new component",
-        title: "title",
-        img: "https://via.placeholder.com/150",
-        content: "new content",
-      },
+      new ComponentModel(
+        "new component",
+        "title",
+        "https://via.placeholder.com/150",
+        "new content",
+        "home"
+      ),
+      new ComponentModel(
+        "Home",
+        "https://hoteliilidza.ba/wp-content/uploads/2017/02/mmm.jpg",
+        "title",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tincidunt ex. Aenean feugiat justo ut mi posuere varius. Maecenas nunc est, fringilla id nisl ut, mollis fermentum lacus. Nunc posuere, diam mattis maximus pharetra, ligula elit pulvinar augue, in feugiat sem massa quis dolor. Nam quis diam dui. Integer malesuada condimentum dolor, nec feugiat justo vestibulum at. Nunc sodales nisl eget lacus facilisis suscipit. Fusce quis placerat felis. Cras faucibus accumsan ante ac interdum. In hac habitasse platea dictumst. Nunc et ipsum ut justo rutrum pulvinar. Vivamus eget hendrerit magna. Nunc ut ullamcorper erat.",
+        "home"
+      ),
     ],
   },
   reducers: {
